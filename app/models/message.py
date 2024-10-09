0996745d7d93335e82a13a175a681a31bbb44f59
Anand Lahoti <anand.lahoti15@gmail.com>
2024-10-09 16:20:56 +0400

    Message and User model

--- app/models/message.py
+++ app/models/message.py
@@ -1,1 +1,14 @@
-# Message model
+from datetime import datetime
+from pydantic import BaseModel, Field
+from typing import Optional, List
+from user import User
+
+class Message(BaseModel):
+    messageTitle: Optional[str] = Field(None, description="Message title")
+    messageContent: str = Field(..., description="Message content from the sender to the receiver")
+    messageSender: User = Field(..., description="User who sent the message")
+    messageUpdatedAt: datetime = Field(default_factory=datetime.utcnow, description="Date and time when the message was last updated")
+    messageReadAt: Optional[datetime] = Field(None, description="Date and time when the message was read by the receiver")
+    messageFiles: Optional[List[str]] = Field(None, description="List of aws s3 files attached to the message, originally uploaded by the sender on the server")
+    messageReceiver: User = Field(..., description="User who will receive the message")
+    messageCreatedAt: datetime = Field(default_factory=datetime.utcnow, description="Date and time when the message was created")
---
+++ app/models/user.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel, Field, EmailStr, constr
+from typing import Optional
+from datetime import datetime
+
+class User(BaseModel):
+    userFirstName: str = Field(..., description="User's first name")
+    userLastName: Optional[str] = Field(None, description="User's last name")
+    userName: str = Field(..., description="User's unique username for login")
+    userEmail: EmailStr = Field(..., description="User's unique email for login")
+    userPassword: str = Field(..., description="User's password, must have at least 8 characters")
+    createdAt: datetime = Field(default_factory=datetime.utcnow, description="Date and time when the user was created")
+    updatedAt: datetime = Field(default_factory=datetime.utcnow, description="Date and time when the user was last updated")
